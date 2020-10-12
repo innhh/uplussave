c81fc4455f30f9c434a54b08ec26309acd7e4ae5
innhh <innhh@naver.com>
2020-10-13 00:07:53 +0900

    Updates

---
+++ precache-manifest.0bccd7e53702b3a1f4966e87a007f8aa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c7631b9ca839a3f6815d15d5c3e4a61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57485dc7bc0840f2cc6d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6bc338b032bcde85b99a",
+    "url": "/static/js/2.389f3b89.chunk.js"
+  },
+  {
+    "revision": "ca962714ee3537eb863ad2954c33c63e",
+    "url": "/static/js/2.389f3b89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57485dc7bc0840f2cc6d",
+    "url": "/static/js/main.257f48b1.chunk.js"
+  },
+  {
+    "revision": "74adc4480331c81648b9",
+    "url": "/static/js/runtime-main.5c813472.js"
+  },
+  {
+    "revision": "603c8e751580391907d1397299371fb5",
+    "url": "/static/media/idcard_correct.603c8e75.png"
+  },
+  {
+    "revision": "aaf1af9e4eeb297967e0e53c2bd6fcfd",
+    "url": "/static/media/idcard_wrong.aaf1af9e.png"
+  }
+]);

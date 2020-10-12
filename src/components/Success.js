7126d9eafb361871e53f584d34a486c8fe324b6d
innhh <innhh@naver.com>
2020-10-13 00:25:14 +0900

    change message

--- src/components/Success.js
+++ src/components/Success.js
@@ -4,7 +4,7 @@ import styled from "styled-components";
 const Container = styled.div`
     text-align: center;
     margin-top: 3rem;
-    line-height: 3rem;
+    line-height: 2rem;
     font-size: 1.2rem;
     font-weight: lighter;
 `;
@@ -12,8 +12,11 @@ const Container = styled.div`
 const Success = () => {
     return (
         <Container>
-            U+ 알뜰폰 신청이 정상적으로 완료되었습니다<br/>
-            빠른 시일내에 확인 후 연락드리도록 하겠습니다 : )
+            U+ 알뜰폰 신청이<br/>
+            정상적으로 완료되었습니다<br/>
+            <br/>
+            빠른 시일내에 확인 후<br/>
+            연락드리도록 하겠습니다 : )
         </Container>
     );
 };
